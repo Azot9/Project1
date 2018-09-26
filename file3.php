d513e6a25a7dd9f08ac7e4c944aeb3e41a07f6d5
Azot9 <igor@national.org.ua>
2018-09-26 12:04:36 +0300

    add new file

---
+++ file3.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>123</title>
+</head>
+<body>
+  <?php
+  echo "number1<br>";
+   echo "number2<br>";
+  echo "1235566665<br>123123<br>";
+  ?>
+</body>
+</html>
